913435e81be2d11f052bf9d334daf8fb885c4b82
quepena <fishalx6263@gmail.com>
2022-12-14 22:24:28 +0200

    navbar

---
+++ components/NavButtons.tsx
@@ -0,0 +1,28 @@
+const NavButtons = [
+    {
+        label: "Logo",
+        path: "/",
+    },
+    {
+        label: "Home",
+        path: "/",
+    },
+    {
+        label: "Skills",
+        path: "/my-shelves",
+    },
+    {
+        label: "Experience",
+        path: "/my-shelves",
+    },
+    {
+        label: "About",
+        path: "/my-shelves",
+    },
+    {
+        label: "Contacts",
+        path: "/my-shelves",
+    },
+];
+
+export default NavButtons;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,68 @@
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
+
   return (
-    <div className={styles.container}>
+    <div className='page'>
       <div className='menu'>
-        <section>Home</section>
-        <section>Skills</section>
-        <section>Experience</section>
-        <section>About</section>
-        <section>Contacts</section>
+        <a href="#home">Home</a>
+        <a href="#skills">Skills</a>
+        <a href="#experience">Experience</a>
+        <a href="#about">About</a>
+        <a href="#contacts">Contacts</a>
       </div>
 
-      <div className='home'>
-        <div>
-          <div>Hi. I am Beata. The Full-Stack Developer</div>
+      <div className="container">
+        <div className='section'>
+          <div id="home" className='home'>
+            <div className='home-contents'>
+              <div className='header'>Hi. I am Beata.</div>
+              <div className='header'>The Full-Stack Developer.</div>
+              <button className='contact-btn'>Contact me</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='section'>
+          <div id="skills" className='skills'>
+            <div>SKILLS</div>
+            <div className='skills-contents'>
+              <div className='header'>Front-End</div>
+              <div className='header'>The Full-Stack Developer.</div>
+              <button className='contact-btn'>Contact me</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='section'>
+          <div id="experience" className='experience'>
+            <div>EXPERIENCE</div>
+            <div className='skills-contents'>
+              <div className='header'>Front-End</div>
+              <div className='header'>The Full-Stack Developer.</div>
+              <button className='contact-btn'>Contact me</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='section'>
+          <div id="about" className='about'>
+            <div>ABOUT</div>
+            <div className='skills-contents'>
+              <div className='header'>Front-End</div>
+              <div className='header'>The Full-Stack Developer.</div>
+              <button className='contact-btn'>Contact me</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='section'>
+          <div id="contacts" className='contacts'>
+            <div>CONTACTS</div>
+            <div className='skills-contents'>
+              <div className='header'>Front-End</div>
+              <div className='header'>The Full-Stack Developer.</div>
+              <button className='contact-btn'>Contact me</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
