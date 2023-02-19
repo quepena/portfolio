2e811d8ad2e272beeb1500173b46793fc039fca4
quepena <fishalx6263@gmail.com>
2023-02-19 21:44:50 +0200

    little upd

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,7 +6,8 @@ import navButtons from './NavButtons'
 const Header = (props: any) => {
     return (
         <nav>
-            <div>Logo</div>
+            <a><img src="https://see.fontimg.com/api/renderfont4/vmgK9/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzAwMDAwMCIsInQiOjF9/Qi5T/billion-dreams.png" alt="" /></a>
+            <a><img src="https://see.fontimg.com/api/renderfont4/1GpKB/eyJyIjoiZnMiLCJoIjo2NSwidyI6MTAwMCwiZnMiOjY1LCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzAwMDAwMCIsInQiOjF9/Qi5T/the-chairman.png" alt="" /></a>
             <NavBar navButtons={navButtons} />
         </nav>
     )
--- pages/experience.tsx
+++ pages/experience.tsx
@@ -1,14 +1,22 @@
 import { faGithub, faYoutube } from '@fortawesome/free-brands-svg-icons'
-import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+import { faGlobe, faCircleExclamation } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 
 const Experience = () => {
     return (
         <div className='section'>
-            <div className='warning'>Hi! Unfortunately, websites presented on this page are deployed via Railway's free tier. Therefore websites might not work or load some data since free resources end every month, and I have to redeploy every project manually at the beginning of the following month. So, please, <b>check out available videos</b> if you see that websites are not loading. Thanks for your time!</div>
             <div className='experience'>
                 <div className='label'>EXPERIENCE</div>
+                <div className='warning'>
+                    <span className='fa-exclamation'>
+                        <FontAwesomeIcon
+                            icon={faCircleExclamation}
+                            className="element-icon"
+                        />
+                    </span>
+                    Websites presented on this page are deployed via Railway's free tier. Therefore websites might not work or load some data since free resources end every month, and I have to redeploy projects manually. So, please, <b>check out available videos</b> if you see that websites are not loading. Thanks for your time!
+                </div>
                 <div className='experience-contents'>
                     <div className='project'>
                         <div className='project-links'>
