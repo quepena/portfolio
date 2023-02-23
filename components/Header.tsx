4b0b9361200413d76981743d7c43b65adc07a4f8
quepena <fishalx6263@gmail.com>
2023-02-23 15:47:41 +0200

    burger menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,12 +9,20 @@ import navButtons from './NavButtons'
 const Header = (props: any) => {
 
     return (
-        <nav>
-            <Link href="/">
-                <img className='logo' src="https://see.fontimg.com/api/renderfont4/GO3ED/eyJyIjoiZnMiLCJoIjo1MiwidyI6MTAwMCwiZnMiOjUyLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzAwMDAwMCIsInQiOjF9/Qi5T/creattion-demo.png" alt="" />
-            </Link>
-            <NavBar navButtons={navButtons} />
-        </nav>
+        <div>
+            <input className="checkbox" type="checkbox" name="" id="" />
+            <div className="hamburger-lines">
+                <span className="line line1"></span>
+                <span className="line line2"></span>
+                <span className="line line3"></span>
+            </div>
+            <nav className='nav'>
+                <Link href="/">
+                    <img className='logo' src="https://see.fontimg.com/api/renderfont4/GO3ED/eyJyIjoiZnMiLCJoIjo1MiwidyI6MTAwMCwiZnMiOjUyLCJmZ2MiOiIjRkZGRkZGIiwiYmdjIjoiIzAwMDAwMCIsInQiOjF9/Qi5T/creattion-demo.png" alt="" />
+                </Link>
+                <NavBar navButtons={navButtons} />
+            </nav>
+        </div>
     )
 }
 
